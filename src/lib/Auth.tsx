676e408c386127803312d30691ab4e80eb647396
Amir Razin <amir.alrazin@gmail.com>
2021-05-31 16:18:44 +0600

    style: fixed some styling

--- src/lib/Auth.tsx
+++ src/lib/Auth.tsx
@@ -24,9 +24,10 @@ export default function Auth() {
         You need to Login before starting Quiz
       </h1>
 
-      <div>
+      <div className="flex items-center px-2 border-4 border-green-500">
+        <MdEmail className="text-3xl text-green-500"/>
         <input
-          className="px-2 py-2 font-semibold text-gray-800 text-green-800 placeholder-green-800 placeholder-opacity-50 border-4 border-green-500 rounded-sm outline-none "
+          className="px-2 py-2 text-xl font-semibold text-gray-800 text-green-800 placeholder-green-800 rounded-sm outline-none placeholder-opacity-60 "
           type="email"
           placeholder="Your email"
           value={email}
@@ -48,3 +49,10 @@ export default function Auth() {
     </div>
   );
 }
+
+
+
+function MdEmail(props) {
+  return <svg stroke="currentColor" fill="currentColor" strokeWidth={0} viewBox="0 0 24 24" height="1em" width="1em" {...props}><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" /></svg>;
+}
+
